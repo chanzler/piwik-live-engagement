e2ccf0b553308a4cff62b7d8cf2c5e475dd95ddf
Frank Kanzler <f.kanzler@telekom.de>
2014-11-26 16:29:26 +0100

    iniatial

--- API.php
+++ API.php
@@ -77,73 +77,48 @@ class API extends \Piwik\Plugin\API {
 			$origin_dt = new \DateTime("now", $origin_dtz);
     	}
 		$refTime = $origin_dt->format('Y-m-d H:i:s');
-        $directSql = "SELECT COUNT(*)
-                FROM " . \Piwik\Common::prefixTable("log_visit") . "
-                WHERE idsite = ?
-                AND DATE_SUB('".$refTime."', INTERVAL ? MINUTE) < visit_last_action_time
-                AND referer_type = ".Common::REFERRER_TYPE_DIRECT_ENTRY."
-                ";
-        $direct = \Piwik\Db::fetchOne($directSql, array(
+
+        $newSql = "SELECT COUNT(DISTINCT(idvisitor))
+                	FROM " . \Piwik\Common::prefixTable("log_visit") . "
+        			WHERE idsite = ?
+					AND DATE_SUB('".$refTime."', INTERVAL ? MINUTE) < visit_last_action_time
+					AND visitor_returning = 0
+                  ";
+        $new = \Piwik\Db::fetchOne($newSql, array(
             $idSite, $lastMinutes+($timeZoneDiff/60)
         ));
 
-        $searchSql = "SELECT COUNT(*)
-                FROM " . \Piwik\Common::prefixTable("log_visit") . "
-                WHERE idsite = ?
-                AND DATE_SUB('".$refTime."', INTERVAL ? MINUTE) < visit_last_action_time
-                AND referer_type = ".Common::REFERRER_TYPE_SEARCH_ENGINE."
+        $returningSql = "SELECT COUNT(DISTINCT(idvisitor))
+                	FROM " . \Piwik\Common::prefixTable("log_visit") . "
+        			WHERE idsite = ?
+					AND DATE_SUB('".$refTime."', INTERVAL ? MINUTE) < visit_last_action_time
+					AND visitor_returning = 1
                 ";
-        $search = \Piwik\Db::fetchOne($searchSql, array(
+        $returning = \Piwik\Db::fetchOne($returningSql, array(
             $idSite, $lastMinutes+($timeZoneDiff/60)
         ));
-
-        $campaignSql = "SELECT COUNT(*)
-                FROM " . \Piwik\Common::prefixTable("log_visit") . "
-                WHERE idsite = ?
-                AND DATE_SUB('".$refTime."', INTERVAL ? MINUTE) < visit_last_action_time
-                AND referer_type = ".Common::REFERRER_TYPE_CAMPAIGN."
+		// more than 20 visits in the last 30 days
+        $loyalSql = "SELECT COUNT(DISTINCT(idvisitor))
+                	FROM " . \Piwik\Common::prefixTable("log_visit") . "
+        			WHERE idsite = ?
+					AND DATE_SUB('".$refTime."', INTERVAL ? MINUTE) < visit_last_action_time
+        			AND visitor_returning = 1
+					AND visitor_days_since_first >= 30
+					AND (30 * visitor_count_visits) / visitor_days_since_first >=20
+        
                 ";
-        $campaign = \Piwik\Db::fetchOne($campaignSql, array(
+        $loyal = \Piwik\Db::fetchOne($loyalSql, array(
         		$idSite, $lastMinutes+($timeZoneDiff/60)
         ));
         
-        $websiteSql = "SELECT COUNT(*)
-                FROM " . \Piwik\Common::prefixTable("log_visit") . "
-                WHERE idsite = ?
-                AND DATE_SUB('".$refTime."', INTERVAL ? MINUTE) < visit_last_action_time
-                AND referer_type = ".Common::REFERRER_TYPE_WEBSITE."
-                ";
-        $website = \Piwik\Db::fetchOne($websiteSql, array(
-        		$idSite, $lastMinutes+($timeZoneDiff/60)
-        ));
-
-        $socialSql = "SELECT referer_url
-                FROM " . \Piwik\Common::prefixTable("log_visit") . "
-                WHERE idsite = ?
-                AND DATE_SUB('".$refTime."', INTERVAL ? MINUTE) < visit_last_action_time
-                AND referer_type = ".Common::REFERRER_TYPE_WEBSITE."
-                ";
-                
-        $social = \Piwik\Db::fetchAll($socialSql, array(
-        		$idSite, $lastMinutes+($timeZoneDiff/60)
-        ));
-        $socialCount = 0;
-        foreach ($social as &$value) {
-        	if(API::isSocialUrl($value['referer_url'])) $socialCount++;
-        }
-
-        $totalVisits = (int)$direct+$search+$campaign+$website;
-		$directPercentage = ($totalVisits==0)?0:round($direct/$totalVisits*100,2);
-		$searchPercentage = ($totalVisits==0)?0:round($search/$totalVisits*100,2);
-		$campaignPercentage = ($totalVisits==0)?0:round($campaign/$totalVisits*100,2);
-		$websitePercentage = ($totalVisits==0)?0:round(($website-$socialCount)/$totalVisits*100,2);
-		$socialPercentage = ($totalVisits==0)?0:round($socialCount/$totalVisits*100,2);
+        $totalVisits = (int)$new+$returning;
+		$newPercentage = ($totalVisits==0)?0:round($new/$totalVisits*100,2);
+		$returningPercentage = ($totalVisits==0)?0:round(($returning-$loyal)/$totalVisits*100,2);
+		$loyalPercentage = ($totalVisits==0)?0:round($loyal/$totalVisits*100,2);
 		return array(
-			array('id'=>1, 'name'=>Piwik::translate('LiveEngagement_Direct'), 'value'=>$direct, 'percentage'=>str_replace(",", ".", sprintf("%01.2f", $directPercentage))),
-			array('id'=>2, 'name'=>Piwik::translate('LiveEngagement_Search'), 'value'=>$search, 'percentage'=>str_replace(",", ".", sprintf("%01.2f", $searchPercentage))),
-			array('id'=>3, 'name'=>Piwik::translate('LiveEngagement_Campaign'), 'value'=>$campaign, 'percentage'=>str_replace(",", ".", sprintf("%01.2f", $campaignPercentage))),
-			array('id'=>4, 'name'=>Piwik::translate('LiveEngagement_Links'), 'value'=>$website, 'percentage'=>str_replace(",", ".", sprintf("%01.2f", $websitePercentage))), //subtract socials
-			array('id'=>5, 'name'=>Piwik::translate('LiveEngagement_Social'), 'value'=>$socialCount, 'percentage'=>str_replace(",", ".", sprintf("%01.2f", $socialPercentage)))
+			array('id'=>1, 'name'=>Piwik::translate('LiveEngagement_New'), 'value'=>$new, 'percentage'=>str_replace(",", ".", sprintf("%01.2f", $newPercentage))),
+			array('id'=>2, 'name'=>Piwik::translate('LiveEngagement_Returning'), 'value'=>($returning-$loyal), 'percentage'=>str_replace(",", ".", sprintf("%01.2f", $returningPercentage))),
+			array('id'=>3, 'name'=>Piwik::translate('LiveEngagement_Loyal'), 'value'=>$loyal, 'percentage'=>str_replace(",", ".", sprintf("%01.2f", $loyalPercentage))),
 		);
     }
 
